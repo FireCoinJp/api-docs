7970ebd908234e76807db3bd406768815bdcee3c
bo.zhou <zhoubo@huobi.com>
2019-04-15 19:49:45 +0900

    add autoload mode

--- config.rb
+++ config.rb
@@ -26,7 +26,9 @@ ready do
   require './lib/multilang.rb'
 end
 
-activate :sprockets
+# activate :sprockets
+activate :livereload
+
 
 activate :autoprefixer do |config|
   config.browsers = ['last 2 version', 'Firefox ESR']
